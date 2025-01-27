16040d554852e9d8526538a0c313d65f683d856a
Kelly Huang <kellyziyanhuang@gmail.com>
2025-01-27 14:42:22 -0800

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Kelly Huang Portfolio",
   description: "Kelly Huang's Portfolio",
+  icons: {
+    icon: "/favicon.ico",
+  }
 };
 
 export default function RootLayout({ children }) {
