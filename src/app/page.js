01ad1fb6facaab8c09c6c8b1e3d3879a75e740b7
Kelly Huang <kellyziyanhuang@gmail.com>
2025-01-29 12:30:45 -0800

    update coming soon page

--- src/app/page.js
+++ src/app/page.js
@@ -7,21 +7,21 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <div className={styles.nav}>
+      {/* <div className={styles.nav}>
         <NavBar/>
-      </div>
+      </div> */}
       <main className={styles.main}>
         <div className={styles.message}>
           <h1>Coming Soon</h1>
           <p className={styles.crab}>🦀</p>
         </div>
-        <section className={styles.projectGallery}>
+        {/* <section className={styles.projectGallery}>
           <h1>My Latest Projects</h1>
           <Link href="/ProjectOne"><Image src="/images/exutoireCover.png" alt="project one" width={80} height={50}/></Link>
           <Link href="/ProjectTwo"><Image src="/images/hivefiveCover.png" alt="project one" width={80} height={50}/></Link>
-        </section>
+        </section> */}
       </main>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 }
