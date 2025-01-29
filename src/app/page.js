0a39073bf482b6a212e049ba5e0d821b77aeac80
Kelly Huang <kellyziyanhuang@gmail.com>
2025-01-29 12:38:17 -0800

    update under construction page

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,33 @@
-import styles from "./page.module.css";
-import NavBar from "./components/NavBar";
-import Image from "next/image";
-import Link from "next/link";
-import Footer from "./components/Footer";
+// import styles from "./page.module.css";
+// import NavBar from "./components/NavBar";
+// import Image from "next/image";
+// import Link from "next/link";
+// import Footer from "./components/Footer";
+
+// export default function Home() {
+//   return (
+//     <div className={styles.page}>
+//       {/* <div className={styles.nav}>
+//         <NavBar/>
+//       </div> */}
+//       <main className={styles.main}>
+//         <div className={styles.message}>
+//           <h1>Coming Soon</h1>
+//           <p className={styles.crab}>🦀</p>
+//         </div>
+//         {/* <section className={styles.projectGallery}>
+//           <h1>My Latest Projects</h1>
+//           <Link href="/ProjectOne"><Image src="/images/exutoireCover.png" alt="project one" width={80} height={50}/></Link>
+//           <Link href="/ProjectTwo"><Image src="/images/hivefiveCover.png" alt="project one" width={80} height={50}/></Link>
+//         </section> */}
+//       </main>
+//       {/* <Footer/> */}
+//     </div>
+//   );
+// }
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      {/* <div className={styles.nav}>
-        <NavBar/>
-      </div> */}
-      <main className={styles.main}>
-        <div className={styles.message}>
-          <h1>Coming Soon</h1>
-          <p className={styles.crab}>🦀</p>
-        </div>
-        {/* <section className={styles.projectGallery}>
-          <h1>My Latest Projects</h1>
-          <Link href="/ProjectOne"><Image src="/images/exutoireCover.png" alt="project one" width={80} height={50}/></Link>
-          <Link href="/ProjectTwo"><Image src="/images/hivefiveCover.png" alt="project one" width={80} height={50}/></Link>
-        </section> */}
-      </main>
-      {/* <Footer/> */}
-    </div>
-  );
+    <h1>Under Construction</h1>
+  )
 }
