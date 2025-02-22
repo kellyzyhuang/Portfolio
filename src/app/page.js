702f797fdda269e57edceab047d1d8b83afa2941
Kelly Huang <kellyziyanhuang@gmail.com>
2025-02-21 18:57:47 -0800

    update home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,26 @@
-// import styles from "./page.module.css";
-// import NavBar from "./components/NavBar";
-// import Image from "next/image";
-// import Link from "next/link";
-// import Footer from "./components/Footer";
-
-// export default function Home() {
-//   return (
-//     <div className={styles.page}>
-//       <NavBar />
-//       <main className={styles.main}>
-//         <div className={styles.message}>
-//           <h1>Under Construction</h1>
-//           <p>Come back soon for updates!</p>
-//         </div>
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// }
+import styles from "./page.module.css";
+import NavBar from "./components/NavBar";
+import Image from "next/image";
+import Link from "next/link";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <h1>Under Construction!</h1>
-  )
+    <div className={styles.page}>
+      <NavBar />
+      <main className={styles.main}>
+        <div className={styles.message}>
+          <h1>Under Construction</h1>
+          <p>Come back soon for updates!</p>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
+
+// export default function Home() {
+//   return (
+//     <h1>Under Construction!</h1>
+//   )
+// }
