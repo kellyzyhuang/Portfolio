2946707a63dbebb2dfe16e384d7bb409fa2395aa
Kelly Huang <kellyziyanhuang@gmail.com>
2025-03-13 01:19:55 -0700

    add id names to sections

--- src/app/ProjectFour/page.js
+++ src/app/ProjectFour/page.js
@@ -72,7 +72,7 @@ export default function ProjectFour() {
           </div>
         </section>
 
-        <section className={styles.problemSection}>
+        <section id="problem" className={styles.problemSection}>
           <h3>Problem Identification</h3>
           <p>
             Through user surveys and secondary research, 3 key problems were identified:
@@ -99,7 +99,7 @@ export default function ProjectFour() {
           </button>
         </section>
 
-        <section className={styles.userPersonasSection}>
+        <section id="personas" className={styles.userPersonasSection}>
           <h3>User Personas</h3>
           <div className={styles.carousel}>
             <button
@@ -118,14 +118,14 @@ export default function ProjectFour() {
           </div>
         </section>
 
-        <section className={styles.solutionSection}>
+        <section id="solution" className={styles.solutionSection}>
           <h3>Solution</h3>
           <p>
             ReachOut addresses the challenges identified by providing a one-stop platform that integrates seamless tracking and communication tools to streamline collaboration between organization workers and employers. By consolidating tools and resources into a single, user-friendly interface, ReachOut eliminates the need for organization workers to navigate multiple platforms, saving time and reducing frustration. The platform's AI-powered job matching ensures that job seekers are paired with opportunities that align with their skills and experience, addressing the inefficiencies of poorly regulated job boards.
           </p>
         </section>
 
-        <section className={styles.userFlowSection}>
+        <section id="user-flow" className={styles.userFlowSection}>
           <h3>User Flow</h3>
           <div className={styles.figmaEmbed}>
             <iframe
@@ -138,7 +138,7 @@ export default function ProjectFour() {
           <p>Two different user flows were created to accommodate the target user groups of employers and organization workers.</p>
         </section>
 
-        <section className={styles.featuresSection}>
+        <section id="features" className={styles.featuresSection}>
           <h3>Features</h3>
           <div className={styles.featuresContainer}>
             <div className={styles.featuresList}>
@@ -172,13 +172,13 @@ export default function ProjectFour() {
           </div>
         </section>
 
-        <section className={styles.styleGuideSection}>
+        <section id="styles-guide" className={styles.styleGuideSection}>
           <h3>Style Guide</h3>
           <Image src="/images/styletile.jpg" width={900} height={600} alt="Style Tile" />
           <p>The styling of the web app is designed to align with values of trust, connection, and support. By carefully choosing colors and imagery that evoke warmth and security, ReachOut creates an environment where the diverse user base of nonprofit workers, ex-offenders, and employers feels safe and empowered.</p>
         </section>
 
-        <section className={styles.promotionalMaterialsSection}>
+        <section id="promotional-materials" className={styles.promotionalMaterialsSection}>
           <h3>Promotional Materials</h3>
 
           <div className={styles.printedMaterials}>
