7e8be040aec1b3827ec64b8867958fd0be44edfa
Kelly Huang <kellyziyanhuang@gmail.com>
2025-03-18 20:44:02 -0700

    create state variable and event handler for ball

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -2,8 +2,17 @@ import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 import Link from "next/link";
 import styles from "./Footer.module.css"; 
 import Image from "next/image";
+import { useState } from "react"; 
 
 export default function Footer() {
+
+  const [isBouncing, setIsBouncing] = useState(false); 
+
+  const handleDogClick = () => {
+    setIsBouncing(true); 
+    setTimeout(() => setIsBouncing(false), 1000); 
+  }
+
   return (
     <footer className={styles.footer}>
       <div className={styles.left}>
@@ -11,7 +20,14 @@ export default function Footer() {
           <Image src="/images/footer-logo.svg" alt="logo" width={80} height={80} />
         </Link>
         <Image src="images/ball.svg" alt="ball" width={12} height={12} className={styles.ball}/>
-        <Image src="images/pookie-2.svg" alt="dog" width={100} height={100} className={styles.pookie} />
+        <Image 
+          src="images/pookie-2.svg" 
+          alt="dog" 
+          width={100} 
+          height={100} 
+          className={styles.pookie}
+          onClick={handleDogClick}
+        />
       </div>
       <div className={styles.right}>
         <p>Connect with me!</p>
