697dc8e64a485d9be597901b64ad9a3666c2914c
Kelly Huang <kellyziyanhuang@gmail.com>
2025-03-28 19:34:48 -0700

    add speech bubble and make pookie running animation smoother

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -21,39 +21,44 @@ export default function Footer() {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.left}>
+       <div className={styles.logoWrapper}></div>
+         <div className={styles.left}>
+          <Image 
+            src="images/ball.svg" 
+            alt="ball" 
+            width={12} 
+            height={12} 
+            className={`${styles.ball} ${isBouncing ? styles.bounce : ""}`}
+            onClick={handleDogClick}
+          />
         <Link href="/">
           <Image src="/images/footer-logo.svg" alt="logo" width={80} height={80} />
         </Link>
 
-        <Image 
-          src="images/ball.svg" 
-          alt="ball" 
-          width={12} 
-          height={12} 
-          className={`${styles.ball} ${isBouncing ? styles.bounce : ""}`}
-        />
+        <div onClick={handleDogClick} className={styles.pookieWrapper}>
 
-        {!isChasing ? (
-          <>
+          {!isChasing ? (
+            <>
+              <Image 
+                src="images/pookie-2.svg" 
+                alt="dog" 
+                width={100} 
+                height={100} 
+                className={styles.pookie}
+                onClick={handleDogClick}
+              />
+              <p className={styles.blurb}>Play with me!</p>
+            </>
+          ) : (
             <Image 
-              src="images/pookie-2.svg" 
-              alt="dog" 
-              width={100} 
-              height={100} 
-              className={styles.pookie}
-              onClick={handleDogClick}
+            src="/videos/pookie-running.gif"
+            width={200}
+            height={120}
+            alt="dog running"
+            className={styles.pookieRunning}
             />
-          </>
-        ) : (
-          <Image 
-          src="/videos/pookie-running.gif"
-          width={200}
-          height={120}
-          alt="dog running"
-          className={styles.pookieRunning}
-          />
-        )}
+          )}
+        </div>
       </div>
 
       <div className={styles.right}>
